bc9a1494f5a7b57a068841857c6dda2f0b55ea25
yushaku <levanson180200@gmail.com>
2023-04-28 14:44:15 +0700

    move file from old app to global

--- api/test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "company" (
-  "id" uuid NOT NULL,
-  "created_at" timestamptz(0) NOT NULL,
-  "updated_at" timestamptz(0) NOT NULL,
-  "name" varchar(255) NULL,
-  "email" varchar(255) NOT NULL,
-  "password" varchar(255) NOT NULL,
-  CONSTRAINT "company_pkey" PRIMARY KEY ("id")
-);
--- apps/apis/src/main.ts
+++ apps/apis/src/main.ts
@@ -1,21 +1,16 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { APP_PORT = 3001 } = process.env;
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(APP_PORT);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Application is running on: http://localhost:${APP_PORT}/${globalPrefix}`,
   );
 }
 
