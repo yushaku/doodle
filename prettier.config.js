d634aaef4bcfcd072e148e91e6df350a0a203cb6
yushaku <levanson180200@gmail.com>
2023-06-09 14:41:29 +0700

    add tailwind

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
